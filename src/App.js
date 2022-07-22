a90da4e4544eb52814e9013a077a2b38258a3a52
flodalia3 <florinda.dalia3@gmail.com>
2022-07-22 15:21:20 +0200

    add layout and grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+//import bookImg from './books.jpeg';
 import './App.css';
+//import {useState} from 'react'; //hook
+import axios from 'axios';
+import React, { useState } from "react";
 
 function App() {
+  const [editions, setEditions] = useState([]);
+  const loadData = () => {
+    const p = axios.get("http://localhost:8080/edition"); //cambia url
+    p.then((res) => setEditions(res.data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div class="grid-container">
+      <div class="grid-item">id: 1 startDate: 23/09/2022 endDate: 15/10/2022 courseName: classroomName:</div>
+      <div class="grid-item">id: 1 startDate: 23/09/2022 endDate: 15/10/2022 courseName: classroomName:</div>
+      <div class="grid-item">id: 1 startDate: 23/09/2022 endDate: 15/10/2022 courseName: classroomName:</div>
+      <div class="grid-item">id: 1 startDate: 23/09/2022 endDate: 15/10/2022 courseName: classroomName:</div>
     </div>
+    {
+      courses.map( (e) => {
+        return (
+          <div key={e.id} class="grid-item">
+              id: {e.id} startDate: {e.startDate} endDate: {e.endDate} 
+              courseId: {e.courseId} courseName: {e.courseName} 
+              classroomId: {e.classroomId} classroomName: {e.classroomName}
+          </div>
+        )
+      }
+      )
+    }
+    
   );
 }
+
+
 
 export default App;
